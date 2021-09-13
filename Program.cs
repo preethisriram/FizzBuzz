6ac23517dd527768bdc78365451b05e8a88412d3
Preethi Sriram <v-psriram@microsoft.com>
2021-09-12 18:33:53 -0700

    First

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program takes the numbers 1 to 100 and prints Fizz for numbers divisible by 3");
+            Console.WriteLine(" Buzz for numbers divisible by 5");
+            Console.WriteLine("and FizzBuzz for numbers divisible by both 3 and 5");
+            FizzBuzz f = new FizzBuzz();
+            f.fizzbuzz();
+        }
+    }
+}
